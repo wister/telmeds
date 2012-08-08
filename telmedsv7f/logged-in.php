1dc57eae85545459817ac4c42a0893787d7aadb2
Wister <wisterfungfung@gmail.com>
2012-08-07 20:54:50 -0500

    Adding link once logged in

--- telmedsv7f/logged-in.php
+++ telmedsv7f/logged-in.php
@@ -2,4 +2,5 @@
 	<?php printf(__('Welcome, %1$s'), bb_get_profile_link(bb_get_current_user_info( 'name' )));?>
 	<?php bb_admin_link( 'before= | ' );?>
 	| <?php bb_logout_link(); ?>
+	| <a href="http://www.telmeds.org/ver">Ver Clases</a>
 </p>
